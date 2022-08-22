a727a89daebe0167ade564d3995cd71e5ff803cb
Ashutosh Mishra <89376505+AshutoshM29@users.noreply.github.com>
2022-08-22 20:41:51 +0530

    Backend data

---
+++ src/backend/db/users.js
@@ -0,0 +1,19 @@
+import { v4 as uuid } from "uuid";
+import { formatDate } from "../utils/authUtils";
+/**
+ * User Database can be added here.
+ * You can add default users of your wish with different attributes
+ * Every user will have likes (Likes are set to 0 by default), History Array, Playlists Array (added Watch Later Playlist in it by default) by default
+ * */
+
+export const users = [
+  {
+    _id: uuid(),
+    firstName: "Ashutosh",
+    lastName: "Mishra",
+    email: "ashutoshrm01@gmail.com",
+    password: "ashu@1234",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+];
